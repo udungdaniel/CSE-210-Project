3ca277cb63fe0ee8a423ccd35e7609d5cac66621
udungdaniel <danielalfred783@gmail.com>
2025-03-08 18:46:08 +0100

    program.cs file is updated

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine();
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("-------------------------------------------");
+        Console.WriteLine();
+
+         // Prompt for the first name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // Prompt for the last name
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // Display the result in the required format
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+        Console.WriteLine();
     }
 }
